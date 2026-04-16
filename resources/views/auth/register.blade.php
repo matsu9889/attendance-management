59926f692a113ca8a18a467c73654095e6c12995
saori <matsu9789@gmail.com>
2026-04-16 20:42:01 +0900

    register validation message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,14 +14,29 @@
         <div>
             <label for="name">名前</label>
             <input name="name" id="name" type="text" value="{{ old('name') }}">
+            <div class="form__error">
+                @error('name')
+                {{ $message }}
+                @enderror
+            </div>
         </div>
         <div>
             <label for="email">メールアドレス</label>
             <input name="email" id="email" type="email" value="{{ old('email') }}">
+            <div class="form__error">
+                @error('email')
+                {{ $message }}
+                @enderror
+            </div>
         </div>
         <div>
             <label for="password">パスワード</label>
             <input name="password" id="password" type="password" value="{{ old('password') }}">
+            <div class="form__error">
+                @error('password')
+                {{ $message }}
+                @enderror
+            </div>
         </div>
         <div>
             <label for="password_confirmation">パスワード確認</label>
