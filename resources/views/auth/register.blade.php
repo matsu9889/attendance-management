9615e4ddb8cf49e46d6190b2125dd9fc909f84af
saori <matsu9789@gmail.com>
2026-04-23 17:05:54 +0900

    ログイン・会員登録CSS

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,45 +1,45 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+<link rel="stylesheet" href="{{ asset('css/auth.css') }}">
 @endsection
 
 @section('content')
-<form action="/register" method="post" class="">
+<form action="/register" method="post" class="form">
     @csrf
-    <h1>会員登録</h1>
-    <div>
-        <label for="name">名前</label>
-        <input name="name" id="name" type="text" value="{{ old('name') }}">
+    <h1 class="form__title">会員登録</h1>
+    <div class="form__group">
+        <label class="form__label" for="name">名前</label>
+        <input class="form__input" name="name" id="name" type="text" value="{{ old('name') }}">
         <div class="form__error">
             @error('name')
             {{ $message }}
             @enderror
         </div>
     </div>
-    <div>
-        <label for="email">メールアドレス</label>
-        <input name="email" id="email" type="email" value="{{ old('email') }}">
+    <div class="form__group">
+        <label class="form__label" for="email">メールアドレス</label>
+        <input class="form__input" name="email" id="email" type="email" value="{{ old('email') }}">
         <div class="form__error">
             @error('email')
             {{ $message }}
             @enderror
         </div>
     </div>
-    <div>
-        <label for="password">パスワード</label>
-        <input name="password" id="password" type="password" value="{{ old('password') }}">
+    <div class="form__group">
+        <label class="form__label" for="password">パスワード</label>
+        <input class="form__input" name="password" id="password" type="password" value="{{ old('password') }}">
         <div class="form__error">
             @error('password')
             {{ $message }}
             @enderror
         </div>
     </div>
-    <div>
-        <label for="password_confirmation">パスワード確認</label>
-        <input name="password_confirmation" id="password_confirmation" type="password">
+    <div class="form__group">
+        <label class="form__label" for="password_confirmation">パスワード確認</label>
+        <input class="form__input" name="password_confirmation" id="password_confirmation" type="password">
     </div>
-    <button>登録する</button>
-    <a href="/login">ログインはこちら</a>
+    <button class="form__button">登録する</button>
+    <a href="/login" class="form__link">ログインはこちら</a>
 </form>
 @endsection
