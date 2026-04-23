3cfec93d58a1e53d0d5008b177cba5350cab51ef
saori <matsu9789@gmail.com>
2026-04-23 16:57:52 +0900

    ログイン画面CSS

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,28 +5,28 @@
 @endsection
 
 @section('content')
-<form action="/login" method="post" class="">
+<form action="/login" method="post" class="form">
     @csrf
-    <h1>ログイン</h1>
-    <div>
-        <label for="email">メールアドレス</label>
-        <input name="email" id="email" type="email" value="{{ old('email') }}">
+    <h1 class="form__title">ログイン</h1>
+    <div class="form__group">
+        <label class="form__label" for="email">メールアドレス</label>
+        <input class="form__input" name="email" id="email" type="email" value="{{ old('email') }}">
         <div class="form__error">
             @error('email')
             {{ $message }}
             @enderror
         </div>
     </div>
-    <div>
-        <label for="password">パスワード</label>
-        <input name="password" id="password" type="password">
+    <div class="form__group">
+        <label class="form__label" for="password">パスワード</label>
+        <input class="form__input" name="password" id="password" type="password">
         <div class="form__error">
             @error('password')
             {{ $message }}
             @enderror
         </div>
     </div>
-    <button>ログインする</button>
-    <a href="/register">会員登録はこちら</a>
+    <button class="form__button">ログインする</button>
+    <a href="/register" class="form__link">会員登録はこちら</a>
 </form>
 @endsection
