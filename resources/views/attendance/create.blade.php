e1c8640297f75abc5b0ed65d0267cbbf4c069678
saori <matsu9789@gmail.com>
2026-04-24 20:26:35 +0900

    ステータス表示

--- resources/views/attendance/create.blade.php
+++ resources/views/attendance/create.blade.php
@@ -5,4 +5,25 @@
 @endsection
 
 @section('content')
+<div>
+    @if(session('work_status') == '出勤中')
+    <p>出勤中</p>
+    @elseif(session('work_status') == '休憩中')
+    <p>休憩中</p>
+    @elseif(session('work_status') == '退勤済')
+    <p>退勤済</p>
+    @else
+    <p>勤務外</p>
+    @endif
+</div>
+
+<?php
+$date = new DateTime();
+echo $date->format('Y-m-d');
+echo $date->format('H:i')
+?>
+<p>日付</p>
+
+<p>時間</p>
+<button>出勤</button>
 @endsection
