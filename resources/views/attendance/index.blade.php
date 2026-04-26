d53e3f43e6a5669f36787376cdadfd6912b58236
saori <matsu9789@gmail.com>
2026-04-26 14:48:17 +0900

    勤怠一覧ビュー作成

---
+++ resources/views/attendance/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/') }}">
+@endsection
+
+@section('content')
+<h1>勤怠一覧</h1>
+<div>
+    <a href="">前月</a>
+    <p>今月</p>
+    <a href="">翌月</a>
+</div>
+
+<table>
+    <tr>
+        <th>日付</th>
+        <th>出勤</th>
+        <th>退勤</th>
+        <th>休憩</th>
+        <th>合計</th>
+        <th>詳細</th>
+    </tr>
+    <tr>
+        <td></td>
+    </tr>
+</table>
+
+@endsection
