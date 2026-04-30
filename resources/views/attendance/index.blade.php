257cc388117160630bee03d78f4bfc92a15ef01f
saori <matsu9789@gmail.com>
2026-04-30 15:46:33 +0900

    勤怠一覧画面CSS

--- resources/views/attendance/index.blade.php
+++ resources/views/attendance/index.blade.php
@@ -5,32 +5,36 @@
 @endsection
 
 @section('content')
-<h1 class="title">勤怠一覧</h1>
-<div class="month">
-    <a class="month__link" href="">前月</a>
-    <p>今月</p>
-    <a class="month__link" href="">翌月</a>
-</div>
+<div class="content-wrapper">
+    <div class="container">
+        <h1 class="title">勤怠一覧</h1>
+        <div class="month">
+            <a class="month__link" href="">前月</a>
+            <p>今月</p>
+            <a class="month__link" href="">翌月</a>
+        </div>
 
-<table class="table">
-    <tr class="table__header">
-        <th>日付</th>
-        <th>出勤</th>
-        <th>退勤</th>
-        <th>休憩</th>
-        <th>合計</th>
-        <th>詳細</th>
-    </tr>
-    @foreach($attendances as $attendance)
-    <tr class="table__description">
-        <td>{{$attendance->date}}</td>
-        <td>{{$attendance->start_time}}</td>
-        <td>{{$attendance->end_time}}</td>
-        <td>{{$attendance->break_total}}</td>
-        <td>{{$attendance->work_total}}</td>
-        <td><a class="" href="">詳細</a></td>
-    </tr>
-    @endforeach
-</table>
+        <table class="table">
+            <tr class="table__header-group">
+                <th class="table__header">日付</th>
+                <th class="table__header">出勤</th>
+                <th class="table__header">退勤</th>
+                <th class="table__header">休憩</th>
+                <th class="table__header">合計</th>
+                <th class="table__header">詳細</th>
+            </tr>
+            @foreach($attendances as $attendance)
+            <tr class="table__description-group">
+                <td class="table__description">{{$attendance->date}}</td>
+                <td class="table__description">{{$attendance->start_time}}</td>
+                <td class="table__description">{{$attendance->end_time}}</td>
+                <td class="table__description">{{$attendance->break_total}}</td>
+                <td class="table__description">{{$attendance->work_total}}</td>
+                <td class="table__description"><a class="description-link" href="">詳細</a></td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
+</div>
 
 @endsection
