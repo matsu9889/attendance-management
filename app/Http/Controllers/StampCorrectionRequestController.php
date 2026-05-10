39208487dcdfb3018df36c4a48b944b7d0103209
saori <matsu9789@gmail.com>
2026-05-10 14:34:19 +0900

    申請一覧承認取得

--- app/Http/Controllers/StampCorrectionRequestController.php
+++ app/Http/Controllers/StampCorrectionRequestController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Attendance;
+use App\Models\BreakRecord;
+use App\Models\CorrectionRequest;
+use App\Models\CorrectionRequestBreak;
+
 
 class StampCorrectionRequestController extends Controller
 {
-    public function index(){
-        return view('stamp_correction_request.index');
+    public function index()
+    {
+        $attendance = Attendance::where('user_id', auth()->id())
+            ->pluck('id');
+        $pending = CorrectionRequest::whereIn('attendance_id', $attendance)
+            ->where('approval', 0)
+            ->get();
+        $approved = CorrectionRequest::whereIn('attendance_id', $attendance)
+            ->where('approval', 1)
+            ->get();
+
+        return view('stamp_correction_request.index', compact('pending', 'approved'));
     }
 }
