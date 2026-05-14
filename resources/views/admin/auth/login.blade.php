3be8c955b38cf4f13b7a7fdf519fe43d9ee6965a
saori <matsu9789@gmail.com>
2026-05-14 22:42:26 +0900

    管理者ログインview

---
+++ resources/views/admin/auth/login.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+@endsection
+
+@section('content')
+<form action="/login" method="post" class="form">
+    @csrf
+    <h1 class="form__title">管理者ログイン</h1>
+    <div class="form__group">
+        <label class="form__label" for="email">メールアドレス</label>
+        <input class="form__input" name="email" id="email" type="email" value="{{ old('email') }}">
+        <div class="form__error">
+            @error('email')
+            {{ $message }}
+            @enderror
+        </div>
+    </div>
+    <div class="form__group">
+        <label class="form__label" for="password">パスワード</label>
+        <input class="form__input" name="password" id="password" type="password">
+        <div class="form__error">
+            @error('password')
+            {{ $message }}
+            @enderror
+        </div>
+    </div>
+    <button class="form__button">管理者ログインする</button>
+</form>
+@endsection
