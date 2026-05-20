3efa04237123c0b70c9089acbf19bca58de2229e
saori <matsu9789@gmail.com>
2026-05-20 17:22:33 +0900

    休憩テーブル修正

--- database/migrations/2026_04_13_100212_create_break__records.php
+++ database/migrations/2026_04_13_100212_create_break__records.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('break_records', function (Blueprint $table) {
             $table->id();
             $table->foreignId('attendance_id')->constrained('attendance_records');
-            $table->time('start_time');
+            $table->time('start_time')->nullable();
             $table->time('end_time')->nullable();
             $table->timestamps();
         });
