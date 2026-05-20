c42d74e41f2c76736186019cfbb973ce74b78276
saori <matsu9789@gmail.com>
2026-05-20 19:53:22 +0900

    スタッフ一覧view作成

---
+++ resources/views/admin/staff/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/attendancelist.css') }}">
+@endsection
+
+@section('content')
+<div class="content-wrapper">
+    <div class="container">
+        <h1 class="title">スタッフ一覧</h1>
+        <table class="table">
+            <tr class="table__header-group">
+                <th class="table__header">名前</th>
+                <th class="table__header">メールアドレス</th>
+                <th class="table__header">月次勤怠</th>
+            </tr>
+            <tr class="table__description-group">
+                <td class="table__description"></td>
+                <td class="table__description"></td>
+                <td class="table__description">
+                    <a class="description-link" href="">詳細</a>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+@endsection
