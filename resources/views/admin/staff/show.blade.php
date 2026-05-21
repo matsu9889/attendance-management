4252aaaf2335b078f53cfc150fd13882f007a8c5
saori <matsu9789@gmail.com>
2026-05-21 20:56:20 +0900

    スタッフ別勤怠画面遷移

--- resources/views/admin/staff/show.blade.php
+++ resources/views/admin/staff/show.blade.php
@@ -41,7 +41,11 @@
                 <td class="table__description">{{ $attendance ? $attendance->break_total : '' }}</td>
                 <td class="table__description">{{ $attendance ? $attendance->work_total : '' }}</td>
                 <td class="table__description">
+                    @if($attendance)
+                    <a class="description-link" href="/admin/attendance/{{$attendance->id}}">詳細</a>
+                    @else
                     <a class="description-link" href="">詳細</a>
+                    @endif
                 </td>
             </tr>
             @endforeach
