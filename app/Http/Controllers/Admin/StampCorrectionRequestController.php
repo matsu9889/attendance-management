25de1bb428dd994f4f50b59c635c523b6d66c904
saori <matsu9789@gmail.com>
2026-05-24 15:54:29 +0900

    管理者修正申請情報取得

--- app/Http/Controllers/Admin/StampCorrectionRequestController.php
+++ app/Http/Controllers/Admin/StampCorrectionRequestController.php
@@ -7,14 +7,15 @@ use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Models\Attendance;
 use App\Models\CorrectionRequest;
-
-
+use App\Models\CorrectionRequestBreak;
 
 class StampCorrectionRequestController extends Controller
 {
     public function edit($id)
     {
-        $correction_request = CorrectionRequest::find($id);
+        $correction_request = CorrectionRequest::with('breakRecords')
+            ->find($id);
+
         $attendance_id = $correction_request->attendance_id;
         $attendance = Attendance::where('id', $attendance_id)
             ->with('breakRecord')
@@ -23,18 +24,14 @@ class StampCorrectionRequestController extends Controller
         $user_name = $attendance->user->name;
         $date = Carbon::parse($attendance->date)->isoFormat('YYYY年MM月DD日');
 
-        $attendance->start_time = Carbon::parse($attendance->start_time)->format('H:i');
-        $attendance->end_time = Carbon::parse($attendance->end_time)->format('H:i');
+        $correction_request->start_time = Carbon::parse($correction_request->start_time)->format('H:i');
+        $correction_request->end_time = Carbon::parse($correction_request->end_time)->format('H:i');
 
-        foreach ($attendance->breakRecord as $break) {
-            $break->start_time = Carbon::parse($break->start_time)->format('H:i');
-            $break->end_time = Carbon::parse($break->end_time)->format('H:i');
+        foreach ($correction_request->breakRecords as $breakRecord) {
+            $breakRecord->start_time = Carbon::parse($breakRecord->start_time)->format('H:i');
+            $breakRecord->end_time = Carbon::parse($breakRecord->end_time)->format('H:i');
         }
 
-        $approval = CorrectionRequest::where('approval', 0)
-            ->where('attendance_id', $attendance->id)
-            ->first();
-
-        return view('admin.stamp_correction_request.edit', compact('id', 'user_name', 'attendance', 'date', 'approval'));
+        return view('admin.stamp_correction_request.edit', compact('id', 'correction_request', 'user_name', 'attendance', 'date',));
     }
 }
--- resources/views/admin/stamp_correction_request/edit.blade.php
+++ resources/views/admin/stamp_correction_request/edit.blade.php
@@ -22,22 +22,34 @@
                 <div class="label-group">
                     <label class="label" for="attendance">出勤・退勤</label>
                     <div class="input-group">
-                        <div>{{$attendance->start_time}}</div>
+                        <div>{{$correction_request->start_time}}</div>
                         <span>～</span>
-                        <div>{{$attendance->end_time}}</div>
+                        <div>{{$correction_request->end_time}}</div>
                     </div>
                 </div>
-                @foreach($attendance->breakRecord as $breakRecord)
+                @foreach($correction_request->breakRecords as $breakRecord)
                 <div class="label-group">
                     <label class="label" for="break">休憩{{ $loop->iteration == 1 ? '' : $loop->iteration }}</label>
-                    <div class="input-group">{{$breakRecord->start_time}}</div>
-                    <span>～</span>
-                    <div>{{$breakRecord->end_time}}</div>
+                    <div class="input-group">
+                        <p>{{$breakRecord->start_time}}</p>
+                        <span>～</span>
+                        <p>{{$breakRecord->end_time}}</p>
+                    </div>
                 </div>
                 @endforeach
                 <div class="label-group">
+                    <label class="label" for="break">休憩{{ $attendance->breakRecord->count() == 0 ? '' : $attendance->breakRecord->count()+1 }}</label>
+                    <div class="input-group">
+                        <p></p>
+                        <span>～</span>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="label-group">
                     <label class="label" for="comment">備考</label>
-                    <div class="input-group">{{$approval->comment}}</div>
+                    <div class="input-group">
+                        <p>{{$correction_request->comment}}</p>
+                    </div>
                 </div>
             </div>
             <div class="button-area">
