2f8e4b34b1ee54e77e2c461f5d3130d0c3e9b1b6
saori <matsu9789@gmail.com>
2026-05-25 21:28:34 +0900

    勤怠詳細CSS修正

--- resources/views/admin/attendance/show.blade.php
+++ resources/views/admin/attendance/show.blade.php
@@ -1,84 +1,80 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/attendanceshow.css') }}">
+<link rel="stylesheet" href="{{ asset('css/attendance-show.css') }}">
 @endsection
 
 @section('content')
-<div class="content-wrapper">
-    <div class="container">
-        <h1 class="title">勤怠詳細</h1>
-        <form action="/admin/attendance/{{$id}}/correct" method="post">
-            @csrf
-            <div class="form">
-                <div class="label-group">
-                    <label class="label" for="name">名前</label>
-                    <div class="text">{{$user_name}}</div>
-                </div>
-                <div class="label-group">
-                    <label class="label" for="date">日付</label>
-                    <div class="text">{{$date}}</div>
-                </div>
-                <div class="label-group">
-                    <label class="label" for="attendance">出勤・退勤</label>
-                    <div class="input-group">
-                        <div>
-                            <input class="input" type="text" name="attendance_start_time" id="attendance" value="{{$attendance->start_time}}">
-                        </div>
-                        <span>～</span>
-                        <div>
-                            <input class="input" type="text" name="attendance_end_time" id="attendance" value="{{$attendance->end_time}}">
-                        </div>
-                        @if($errors->has('attendance_start_time') || $errors->has('attendance_end_time'))
-                        <p>出勤時間もしくは退勤時間が不適切な値です</p>
-                        @endif
+<div class="container">
+    <h1 class="title">勤怠詳細</h1>
+    <form action="/admin/attendance/{{$id}}/correct" method="post">
+        @csrf
+        <div class="form">
+            <div class="label-group">
+                <label class="label" for="name">名前</label>
+                <div class="text">{{$user_name}}</div>
+            </div>
+            <div class="label-group">
+                <label class="label" for="date">日付</label>
+                <div class="text">{{$date}}</div>
+            </div>
+            <div class="label-group">
+                <label class="label" for="attendance">出勤・退勤</label>
+                <div class="input-group">
+                    <div>
+                        <input class="input-time" type="text" name="attendance_start_time" id="attendance" value="{{$attendance->start_time}}">
                     </div>
-                </div>
-                @foreach($attendance->breakRecord as $breakRecord)
-                <div class="label-group">
-                    <label class="label" for="break">休憩{{ $loop->iteration == 1 ? '' : $loop->iteration }}</label>
-                    <div class="input-group">
-                        <input class="input" type="text" name="break_start_time[]" id="break" value="{{$breakRecord->start_time}}">
-                        <span>～</span>
-                        <input class="input" type="text" name="break_end_time[]" id="break" value="{{$breakRecord->end_time}}">
-                        @if($errors->has('break_start_time.*') || $errors->has('break_end_time.*'))
-                        <p>休憩時間が不適切な値です</p>
-                        @endif
+                    <span>～</span>
+                    <div>
+                        <input class="input-time" type="text" name="attendance_end_time" id="attendance" value="{{$attendance->end_time}}">
                     </div>
-                </div>
-                @endforeach
-                <div class="label-group">
-                    <label class="label" for="break">休憩{{ $attendance->breakRecord->count() == 0 ? '' : $attendance->breakRecord->count()+1 }}</label>
-                    <div class="input-group">
-                        <input class="input" type="text" name="break_start_time[]" id="break" value="">
-                        <span>～</span>
-                        <input class="input" type="text" name="break_end_time[]" id="break" value="">
-                        @if($errors->has('break_start_time.*') || $errors->has('break_end_time.*'))
-                        <p>休憩時間が不適切な値です</p>
-                        @endif
-                    </div>
-                </div>
-                <div class="label-group">
-                    <label class="label" for="comment">備考</label>
-                    <div class="input-group">
-                        <textarea class="textarea" name="comment" id="comment" value=""></textarea>
-                        <div>
-                            @error('comment')
-                            <p>{{ $message }}</p>
-                            @enderror
-                        </div>
-                    </div>
+                    @if($errors->has('attendance_start_time') || $errors->has('attendance_end_time'))
+                    <p>出勤時間もしくは退勤時間が不適切な値です</p>
+                    @endif
                 </div>
             </div>
-            @if($approval == true)
-            <p class="message">*承認待ちのため修正はできません。</p>
-            @else
-            <div class="button-area">
-                <button class="button">修正</button>
+            @foreach($attendance->breakRecord as $breakRecord)
+            <div class="label-group">
+                <label class="label" for="break">休憩{{ $loop->iteration == 1 ? '' : $loop->iteration }}</label>
+                <div class="input-group">
+                    <input class="input-time" type="text" name="break_start_time[]" id="break" value="{{$breakRecord->start_time}}">
+                    <span>～</span>
+                    <input class="input-time" type="text" name="break_end_time[]" id="break" value="{{$breakRecord->end_time}}">
+                    @if($errors->has('break_start_time.*') || $errors->has('break_end_time.*'))
+                    <p>休憩時間が不適切な値です</p>
+                    @endif
+                </div>
             </div>
-            @endif
-        </form>
-    </div>
+            @endforeach
+            <div class="label-group">
+                <label class="label" for="break">休憩{{ $attendance->breakRecord->count() == 0 ? '' : $attendance->breakRecord->count()+1 }}</label>
+                <div class="input-group">
+                    <input class="input-time" type="text" name="break_start_time[]" id="break" value="">
+                    <span>～</span>
+                    <input class="input-time" type="text" name="break_end_time[]" id="break" value="">
+                    @if($errors->has('break_start_time.*') || $errors->has('break_end_time.*'))
+                    <p>休憩時間が不適切な値です</p>
+                    @endif
+                </div>
+            </div>
+            <div class="label-group">
+                <label class="label" for="comment">備考</label>
+                <div class="input-group">
+                    <textarea class="textarea" name="comment" id="comment">{{ old('comment') }}</textarea>
+                    @error('comment')
+                    <p>{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+        </div>
+        @if($approval == true)
+        <p class="message">*承認待ちのため修正はできません。</p>
+        @else
+        <div class="button-area">
+            <button class="button">修正</button>
+        </div>
+        @endif
+    </form>
 </div>
 
 @endsection
