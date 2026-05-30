ddd3f6dbbc7d2874500c39df47f3aeb164491c2e
saori <matsu9789@gmail.com>
2026-05-30 16:41:27 +0900

    勤怠承認機能修正

--- app/Http/Controllers/Admin/StampCorrectionRequestController.php
+++ app/Http/Controllers/Admin/StampCorrectionRequestController.php
@@ -39,9 +39,8 @@ class StampCorrectionRequestController extends Controller
     public function update($attendance_correct_request_id)
     {
         $correction_request = CorrectionRequest::with('breakRecords')
-        ->find($attendance_correct_request_id);
-        dd($correction_request);
-        
+            ->find($attendance_correct_request_id);
+
         $correction_request->update([
             'approval' => '1'
         ]);
@@ -49,24 +48,21 @@ class StampCorrectionRequestController extends Controller
         $attendance_id = $correction_request->attendance_id;
         if ($correction_request->start_time !== null) {
             Attendance::where('id', $attendance_id)
-            ->update([
-                'start_time' => $correction_request->start_time,
-                'end_time' => $correction_request->end_time
-            ]);
-        }
-
-        $breakRecord = breakRecord::where('attendance_id',$attendance_id);
-        dd($breakRecord);
-
-        foreach ($correction_request->breakRecords as $correctionBreakRecordd) { //休憩修正テーブル
-            BreakRecord::where('attendance_id', $attendance_id)
-                ->where('correction_request_id', $correction_request->id)
                 ->update([
-                    'start_time' => $breakRecord->start_time,
-                    'end_time' => $breakRecord->end_time
+                    'start_time' => $correction_request->start_time,
+                    'end_time' => $correction_request->end_time
                 ]);
         }
 
+        BreakRecord::where('attendance_id', $attendance_id)->delete();
+
+        foreach ($correction_request->breakRecords as $correctionRequestBreakRecord) {
+            BreakRecord::create([
+                'attendance_id' => $attendance_id,
+                'start_time' => $correctionRequestBreakRecord->start_time,
+                'end_time' => $correctionRequestBreakRecord->end_time,
+            ]);
+        }
         return redirect('/stamp_correction_request/list');
     }
 }
