9325822bf62041f9cce85f34d63ffc5216a74bd7
saori <matsu9789@gmail.com>
2026-06-01 18:05:28 +0900

    不要なタグ削除

--- resources/views/admin/stamp_correction_request/edit.blade.php
+++ resources/views/admin/stamp_correction_request/edit.blade.php
@@ -5,65 +5,63 @@
 @endsection
 
 @section('content')
-<div class="content-wrapper">
-    <div class="container">
-        <h1 class="title">勤怠詳細</h1>
-        <form action="/admin/stamp_correction_request/approve/{{$id}}" method="post">
-            @csrf
-            <div class="form">
-                <div class="label-group">
-                    <label class="label" for="name">名前</label>
-                    <div class="text">{{$user_name}}</div>
-                </div>
-                <div class="label-group">
-                    <label class="label" for="date">日付</label>
-                    <div class="input-group">
-                        <div class="date">{{$year}}</div>
-                        <span>&emsp;</span>
-                        <div class="date">{{$date}}</div>
-                    </div>
-                </div>
-                <div class="label-group">
-                    <label class="label" for="attendance">出勤・退勤</label>
-                    <div class="input-group">
-                        <p class="time">{{$correction_request->start_time}}</p>
-                        <span>～</span>
-                        <p class="time">{{$correction_request->end_time}}</p>
-                    </div>
-                </div>
-                @foreach($correction_request->breakRecords as $breakRecord)
-                <div class="label-group">
-                    <label class="label" for="break">休憩{{ $loop->iteration == 1 ? '' : $loop->iteration }}</label>
-                    <div class="input-group">
-                        <p class="time">{{$breakRecord->start_time}}</p>
-                        <span>～</span>
-                        <p class="time">{{$breakRecord->end_time}}</p>
-                    </div>
-                </div>
-                @endforeach
-                <div class="label-group">
-                    <label class="label" for="break">休憩{{ $attendance->breakRecord->count() == 0 ? '' : $attendance->breakRecord->count()+1 }}</label>
-                    <div class="input-group">
-                        <p></p>
-                        <p></p>
-                    </div>
-                </div>
-                <div class="label-group">
-                    <label class="label" for="comment">備考</label>
-                    <div class="input-group">
-                        <p class="text">{{$correction_request->comment}}</p>
-                    </div>
+<div class="container">
+    <h1 class="title">勤怠詳細</h1>
+    <form action="/admin/stamp_correction_request/approve/{{$id}}" method="post">
+        @csrf
+        <div class="form">
+            <div class="label-group">
+                <label class="label" for="name">名前</label>
+                <div class="text">{{$user_name}}</div>
+            </div>
+            <div class="label-group">
+                <label class="label" for="date">日付</label>
+                <div class="input-group">
+                    <div class="date">{{$year}}</div>
+                    <span>&emsp;</span>
+                    <div class="date">{{$date}}</div>
                 </div>
             </div>
-            <div class="button-area">
-                @if($correction_request->approval === 0)
-                <button class="button">承認</button>
-                @else
-                <button class="button-approval">承認済み</button>
-                @endif
+            <div class="label-group">
+                <label class="label" for="attendance">出勤・退勤</label>
+                <div class="input-group">
+                    <p class="time">{{$correction_request->start_time}}</p>
+                    <span>～</span>
+                    <p class="time">{{$correction_request->end_time}}</p>
+                </div>
             </div>
-        </form>
-    </div>
+            @foreach($correction_request->breakRecords as $breakRecord)
+            <div class="label-group">
+                <label class="label" for="break">休憩{{ $loop->iteration == 1 ? '' : $loop->iteration }}</label>
+                <div class="input-group">
+                    <p class="time">{{$breakRecord->start_time}}</p>
+                    <span>～</span>
+                    <p class="time">{{$breakRecord->end_time}}</p>
+                </div>
+            </div>
+            @endforeach
+            <div class="label-group">
+                <label class="label" for="break">休憩{{ $attendance->breakRecord->count() == 0 ? '' : $attendance->breakRecord->count()+1 }}</label>
+                <div class="input-group">
+                    <p></p>
+                    <p></p>
+                </div>
+            </div>
+            <div class="label-group">
+                <label class="label" for="comment">備考</label>
+                <div class="input-group">
+                    <p class="text">{{$correction_request->comment}}</p>
+                </div>
+            </div>
+        </div>
+        <div class="button-area">
+            @if($correction_request->approval === 0)
+            <button class="button">承認</button>
+            @else
+            <button class="button-approval">承認済み</button>
+            @endif
+        </div>
+    </form>
 </div>
 
 @endsection
