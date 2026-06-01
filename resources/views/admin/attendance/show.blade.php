37ba67d5db9164af9b590c29b28d1f859d09f0de
saori <matsu9789@gmail.com>
2026-06-01 18:09:28 +0900

    エラー表示変更

--- resources/views/admin/attendance/show.blade.php
+++ resources/views/admin/attendance/show.blade.php
@@ -22,48 +22,56 @@
                 <label class="label" for="attendance">出勤・退勤</label>
                 <div class="input-group">
                     <div>
-                        <input class="input-time" type="text" name="attendance_start_time" id="attendance" value="{{$attendance->start_time}}">
+                        <input class="input-time" type="text" name="attendance_start_time" value="{{$attendance->start_time}}">
                     </div>
                     <span>～</span>
                     <div>
-                        <input class="input-time" type="text" name="attendance_end_time" id="attendance" value="{{$attendance->end_time}}">
+                        <input class="input-time" type="text" name="attendance_end_time" value="{{$attendance->end_time}}">
                     </div>
-                    @if($errors->has('attendance_start_time') || $errors->has('attendance_end_time'))
-                    <p>出勤時間もしくは退勤時間が不適切な値です</p>
-                    @endif
+                    <div class="input-error">
+                        @if($errors->has('attendance_start_time') || $errors->has('attendance_end_time'))
+                        <p>出勤時間もしくは退勤時間が不適切な値です</p>
+                        @endif
+                    </div>
                 </div>
             </div>
             @foreach($attendance->breakRecord as $breakRecord)
             <div class="label-group">
                 <label class="label" for="break">休憩{{ $loop->iteration == 1 ? '' : $loop->iteration }}</label>
                 <div class="input-group">
-                    <input class="input-time" type="text" name="break_start_time[]" id="break" value="{{$breakRecord->start_time}}">
+                    <input class="input-time" type="text" name="break_start_time[]" value="{{$breakRecord->start_time}}">
                     <span>～</span>
-                    <input class="input-time" type="text" name="break_end_time[]" id="break" value="{{$breakRecord->end_time}}">
-                    @if($errors->has('break_start_time.*') || $errors->has('break_end_time.*'))
-                    <p>休憩時間が不適切な値です</p>
-                    @endif
+                    <input class="input-time" type="text" name="break_end_time[]" value="{{$breakRecord->end_time}}">
+                    <div class="input-error">
+                        @if($errors->has('break_start_time.*') || $errors->has('break_end_time.*'))
+                        <p>休憩時間が不適切な値です</p>
+                        @endif
+                    </div>
                 </div>
             </div>
             @endforeach
             <div class="label-group">
                 <label class="label" for="break">休憩{{ $attendance->breakRecord->count() == 0 ? '' : $attendance->breakRecord->count()+1 }}</label>
                 <div class="input-group">
-                    <input class="input-time" type="text" name="break_start_time[]" id="break" value="">
+                    <input class="input-time" type="text" name="break_start_time[]" value="">
                     <span>～</span>
-                    <input class="input-time" type="text" name="break_end_time[]" id="break" value="">
-                    @if($errors->has('break_start_time.*') || $errors->has('break_end_time.*'))
-                    <p>休憩時間が不適切な値です</p>
-                    @endif
+                    <input class="input-time" type="text" name="break_end_time[]" value="">
+                    <div class="input-error">
+                        @if($errors->has('break_start_time.*') || $errors->has('break_end_time.*'))
+                        <p>休憩時間が不適切な値です</p>
+                        @endif
+                    </div>
                 </div>
             </div>
             <div class="label-group">
                 <label class="label" for="comment">備考</label>
                 <div class="input-group">
                     <textarea class="textarea" name="comment" id="comment">{{ old('comment') }}</textarea>
-                    @error('comment')
-                    <p>{{ $message }}</p>
-                    @enderror
+                    <div class="input-error">
+                        @error('comment')
+                        <p>{{ $message }}</p>
+                        @enderror
+                    </div>
                 </div>
             </div>
         </div>
